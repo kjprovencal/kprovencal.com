ec79327e9452c273a396488c19dfba860861a76e
kjprovencal <kjprovencal@gmail.com>
2025-03-02 23:01:31 -0500

    Fix skill rounded size

--- app/resume/skills.tsx
+++ app/resume/skills.tsx
@@ -7,7 +7,7 @@ function SkillRow({ data }: { data: SkillsEntry }) {
     <li key={data.name} className="relative mb-[60px] bg-pakistan dark:bg-cornsilk h-10 rounded-xs">
       <em className="font-open_sans font-bold uppercase not-italic text-sm tracking-widest relative -top-9">{data.name}</em>
       <span
-        className={"absolute bg-tiger left-0 top-0 m-0 pr-6 inline-block h-10 leading-10 rounded-l-sm"}
+        className={"absolute bg-tiger left-0 top-0 m-0 pr-6 inline-block h-10 leading-10 rounded-xs"}
         style={{ width: `${data.level}%`}}
       />
     </li>
